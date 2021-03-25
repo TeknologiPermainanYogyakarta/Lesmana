5c4885ef985535bac719bc7bead5e0dd8bbf4aaa
hassanshin <f3fahmi@gmail.com>
2021-03-25 20:08:10 +0700

    -fix tutorial bug -add cinemachine

--- Assets/Scripts/Model/PlatformerModel.cs
+++ Assets/Scripts/Model/PlatformerModel.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 namespace Platformer.Model
 {
     /// <summary>
-    /// The main model containing needed data to implement a platformer style 
-    /// game. This class should only contain data, and methods that operate 
+    /// The main model containing needed data to implement a platformer style
+    /// game. This class should only contain data, and methods that operate
     /// on the data. It is initialised with data in the GameController class.
     /// </summary>
     [System.Serializable]
     public class PlatformerModel
     {
-        /// <summary>
-        /// The virtual camera in the scene.
-        /// </summary>
+        ///// <summary>
+        ///// The virtual camera in the scene.
+        ///// </summary>
         public Cinemachine.CinemachineVirtualCamera virtualCamera;
 
         /// <summary>
-        /// The main component which controls the player sprite, controlled 
+        /// The main component which controls the player sprite, controlled
         /// by the user.
         /// </summary>
         public PlayerController player;
@@ -33,10 +33,9 @@ namespace Platformer.Model
         public float jumpModifier = 1.5f;
 
         /// <summary>
-        /// A global jump modifier applied to slow down an active jump when 
+        /// A global jump modifier applied to slow down an active jump when
         /// the user releases the jump input.
         /// </summary>
         public float jumpDeceleration = 0.5f;
-
     }
 }
--- Assets/Tutorials/BuildAndPublish/Criteria/PublishCriteria.cs
+++ Assets/Tutorials/BuildAndPublish/Criteria/PublishCriteria.cs
@@ -1,5 +1,6 @@
 using System.Linq;
-using Unity.Play.Publisher.Editor;
+
+//using Unity.Play.Publisher.Editor;
 using UnityEngine;
 
 namespace Unity.Tutorials
@@ -7,63 +8,63 @@ namespace Unity.Tutorials
     /// <summary>
     /// Contaisn all the callbacks needed for the Build And Publish tutorial
     /// </summary>
-    [CreateAssetMenu(fileName = "PublishCriteria", menuName = "Tutorials/Microgame/PublishCriteria")]
-    class PublishCriteria : ScriptableObject
-    {
-        static PublisherWindow publisherWindow;
-        public bool IsNotDisplayingFirstTimeInstructions()
-        {
-            if (!IsWebGLPublisherOpen()) { return false; }
-            return (!string.IsNullOrEmpty(publisherWindow.CurrentTab) && publisherWindow.CurrentTab != PublisherWindow.TabIntroduction);
-        }
+    //[CreateAssetMenu(fileName = "PublishCriteria", menuName = "Tutorials/Microgame/PublishCriteria")]
+    //class PublishCriteria : ScriptableObject
+    //{
+    //    static PublisherWindow publisherWindow;
+    //    public bool IsNotDisplayingFirstTimeInstructions()
+    //    {
+    //        if (!IsWebGLPublisherOpen()) { return false; }
+    //        return (!string.IsNullOrEmpty(publisherWindow.CurrentTab) && publisherWindow.CurrentTab != PublisherWindow.TabIntroduction);
+    //    }
 
-        public bool IsUserLoggedIn()
-        {
-            if (!IsWebGLPublisherOpen()) { return false; }
-            return (publisherWindow.CurrentTab != PublisherWindow.TabNotLoggedIn);
-        }
+    //    public bool IsUserLoggedIn()
+    //    {
+    //        if (!IsWebGLPublisherOpen()) { return false; }
+    //        return (publisherWindow.CurrentTab != PublisherWindow.TabNotLoggedIn);
+    //    }
 
-        public bool IsBuildBeingUploaded()
-        {
-            if (!IsWebGLPublisherOpen()) { return false; }
-            switch (PublisherUtils.GetCurrentPublisherState(publisherWindow))
-            {
-                case PublisherState.Upload:
-                case PublisherState.Process:
-                    return true;
-                default: break;
-            }
-            return !string.IsNullOrEmpty(PublisherUtils.GetUrlOfLastPublishedBuild(publisherWindow));
-        }
+    //    public bool IsBuildBeingUploaded()
+    //    {
+    //        if (!IsWebGLPublisherOpen()) { return false; }
+    //        switch (PublisherUtils.GetCurrentPublisherState(publisherWindow))
+    //        {
+    //            case PublisherState.Upload:
+    //            case PublisherState.Process:
+    //                return true;
+    //            default: break;
+    //        }
+    //        return !string.IsNullOrEmpty(PublisherUtils.GetUrlOfLastPublishedBuild(publisherWindow));
+    //    }
 
-        public bool IsBuildPublished()
-        {
-            if (!IsWebGLPublisherOpen()) { return false; }
-            return !string.IsNullOrEmpty(PublisherUtils.GetUrlOfLastPublishedBuild(publisherWindow));
-        }
+    //    public bool IsBuildPublished()
+    //    {
+    //        if (!IsWebGLPublisherOpen()) { return false; }
+    //        return !string.IsNullOrEmpty(PublisherUtils.GetUrlOfLastPublishedBuild(publisherWindow));
+    //    }
 
-        public bool AtLeastOneBuildIsRegistered()
-        {
-            if (!IsWebGLPublisherOpen()) { return false; }
-            switch (PublisherUtils.GetCurrentPublisherState(publisherWindow))
-            {
-                case PublisherState.Zip:
-                case PublisherState.Upload:
-                case PublisherState.Process:
-                    return true;
-                default: break;
-            }
-            int availableBuilds = PublisherUtils.GetAllBuildsDirectories()
-                                            .Where(p => (p != string.Empty)
-                                                        && PublisherUtils.BuildIsValid(p)).Count();
-            return availableBuilds > 0;
-        }
+    //    public bool AtLeastOneBuildIsRegistered()
+    //    {
+    //        if (!IsWebGLPublisherOpen()) { return false; }
+    //        switch (PublisherUtils.GetCurrentPublisherState(publisherWindow))
+    //        {
+    //            case PublisherState.Zip:
+    //            case PublisherState.Upload:
+    //            case PublisherState.Process:
+    //                return true;
+    //            default: break;
+    //        }
+    //        int availableBuilds = PublisherUtils.GetAllBuildsDirectories()
+    //                                        .Where(p => (p != string.Empty)
+    //                                                    && PublisherUtils.BuildIsValid(p)).Count();
+    //        return availableBuilds > 0;
+    //    }
 
-        bool IsWebGLPublisherOpen()
-        {
-            if (publisherWindow) { return true; }
-            publisherWindow = PublisherWindow.FindInstance();
-            return false;
-        }
-    }
+    //    bool IsWebGLPublisherOpen()
+    //    {
+    //        if (publisherWindow) { return true; }
+    //        publisherWindow = PublisherWindow.FindInstance();
+    //        return false;
+    //    }
+    //}
 }
