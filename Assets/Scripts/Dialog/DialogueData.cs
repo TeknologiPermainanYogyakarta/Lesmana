5f20ba92cedf711539997baa0e2240a492bf2257
hassanshin <f3fahmi@gmail.com>
2021-03-28 14:29:19 +0700

    -dialogue done

---
+++ Assets/Scripts/Dialog/DialogueData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialgoueData", menuName = "ScriptableObjects/New DialgoueData", order = 1)]
+[System.Serializable]
+public class DialogueMasterData : ScriptableObject
+{
+    public List<LevelStoryData> StoryList = new List<LevelStoryData>();
+}
+
+[System.Serializable]
+
+public class LevelStoryData
+{
+    public string StoryName;
+    public List<ChapterData> Chapter = new List<ChapterData>();
+}
+
+[System.Serializable]
+
+public class ChapterData
+{
+    public string chapterName;
+    public List<ChatData> Chat = new List<ChatData>();
+}
+
+[System.Serializable]
+public class ChatData
+{
+    public string chat;
+    public string actorName;
+    public bool isOnRightSide;
+}
---
+++ Assets/Scripts/UI/DialogueController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueController : MonoBehaviour
+{
+    [SerializeField]
+    private Image panel = null;
+
+    [SerializeField]
+    private TextMeshProUGUI chat = null;
+
+    [SerializeField]
+    private TextMeshProUGUI actor = null;
+
+    [SerializeField]
+    private Button nextButton = null;
+
+    private void Start()
+    {
+        nextButton.onClick.AddListener(() =>
+        {
+            StoryManager.Instance.NextDialogue();
+        });
+    }
+
+    public void StartDialogue()
+    {
+        panel.gameObject.SetActive(true);
+    }
+
+    public void SetText(string _text, string _actor)
+    {
+        chat.text = _text;
+        actor.text = _actor;
+    }
+
+    public void CloseDialogue()
+    {
+        panel.gameObject.SetActive(false);
+    }
+}
